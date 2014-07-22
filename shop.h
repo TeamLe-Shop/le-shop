9d3b1fd89eb793a15a32987c7f7690b42a508290
Mika Attila <radiantstatue@gmail.com>
2014-07-22 22:57:00 +0200

    Remove needless extern item list from shop.h

--- shop.h
+++ shop.h
@@ -6,12 +6,6 @@
 
 extern int item_count;
 
-/* ITEM LIST */
-extern item_t cat;
-extern item_t shoes;
-extern item_t bread;
-extern item_t wine;
-
 item_t* item_list;
 
 /* Initializes the items and the item list. */

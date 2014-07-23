8ffe33fb4388f80e999686071088865267daf53f
Sebbyastian <plebbyastian@gmail.com>
2014-07-24 06:27:24 +0930

    Refactored money away from item

--- item.c
+++ item.c
@@ -2,23 +2,10 @@
 
 void print_item(char* str, item_t item)
 {
-	char istr[3];
-
-	int cents   = item.price % 100;
-	long int dollars = item.price / 100;
-
-	if (cents < 10) sprintf(istr, "0%i", cents);
-	else sprintf(istr, "%i", cents);
-	sprintf(str, "%-*s| $%li.%s", MAX_NAME_LEN, item.name, dollars, istr);
+	sprintf(str, "%-*s| " MONEY_FMT, MAX_NAME_LEN, item.name, MONEY_ARG(item.price));
 }
 
 void print_money(char* str, long int price)
 {
-	int cents = price % 100;
-	long int dollars = price / 100;
-	
-	if (cents < 10)
-	{
-		sprintf(str, "%li.0%i", dollars, cents);
-	} else sprintf(str, "%li.%i", dollars, cents);
+	sprintf(str, MONEY_FMT, MONEY_ARG(price));
 }
--- item.h
+++ item.h
@@ -33,4 +33,18 @@ void print_item(char*, item_t);
  */
 void print_money(char*, long int price);
 
+/* Bear with me through this preprocessor macro stuff
+ */
+
+#define getDigits(num) (1                                   /* 1 for sign */ \
+                      + sizeof (num) * CHAR_BIT / 3         /* ... for digits */ \
+                      + (sizeof (num) * CHAR_BIT % 3 > 0)   /* ... for remaining digit */ \
+                      + 1)                                  /* 1 for NUL terminator */
+
+#define MONEY_FMT    "$%*li.%2li"
+#define MONEY_ARG(p) getDigits((p) / 100), (p) / 100, (p) % 100
+
+/* Now you can use MONEY_FMT and MONEY_ARG in any *printf function
+ */
+
 #endif /* ITEM_H */
